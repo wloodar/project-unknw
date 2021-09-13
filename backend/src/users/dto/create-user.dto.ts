8b4310705f83b1799ef72e26dcd39a6703759db6
Wlodar <wlodardev@gmail.com>
2021-09-13 19:13:44 +0200

    Users controller added

---
+++ backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import { IsNotEmpty, IsEmail, IsString, MinLength , MaxLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @MaxLength(32)
+    @IsNotEmpty()
+    readonly firstName: string
+
+    @IsString()
+    @MaxLength(32)
+    @IsNotEmpty()
+    readonly lastName: string
+
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
+    readonly email: string;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(32)
+    @IsNotEmpty()
+    readonly password: string;
+}
